3a32371c1bedaa871cdbc99f90f52d7a1650daaa
BradenEverson <bradeneverson@gmail.com>
2025-09-03 05:30:05 -0500

    We try a little driving

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 //! Main Chair Runtime
 
+use std::{thread, time::Duration};
+
 use driveable_chair::odrive::OdriveController;
 
-fn main() {
-    for port in serialport::available_ports().expect("No serial ports") {
-        println!("{port:?}");
-    }
+/// Serial port of the right odrive wheel
+const RIGHT_WHEEL: &'static str = "/dev/ttyACM0";
 
-    let odrive = OdriveController::from_port("foo").expect("No odrive :(");
+fn main() {
+    let mut right_odrive = OdriveController::from_port(RIGHT_WHEEL).expect("No odrive :(");
+
+    right_odrive
+        .velocity(0, 10, None)
+        .expect("Failed to set velocity");
+
+    thread::sleep(Duration::from_secs(5));
+
+    right_odrive
+        .velocity(0, 0, None)
+        .expect("Failed to set velocity");
 }
--- src/odrive.rs
+++ src/odrive.rs
@@ -22,4 +22,21 @@ impl OdriveController {
 
         Ok(Self { port })
     }
+
+    /// Sends the velocity setting command
+    pub fn velocity(
+        &mut self,
+        motor: u8,
+        velocity: u16,
+        torque_ff: Option<u16>,
+    ) -> std::io::Result<()> {
+        let cmd = if let Some(torque_ff) = torque_ff {
+            format!("v {motor} {velocity} {torque_ff}")
+        } else {
+            format!("v {motor} {velocity}")
+        };
+
+        self.port.write(cmd.as_bytes())?;
+        self.port.flush()
+    }
 }
